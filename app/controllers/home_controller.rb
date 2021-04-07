470b963c334b7f892640e2752378952d063e6d8e
Enrico Mucelli <e.mucelli@donboscosandona.it>
2021-04-07 09:33:48 +0000

    ultimato navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
     def index
     end
+
+    def bibbione
+    end
+
+    def paluda
+    end
+
+    def jesolo
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/index', to: 'home#index', as: 'index'
+  get '/bibbione', to: 'home#bibbione', as: 'bibbione'
+  get '/paluda', to: 'home#paluda', as: 'paluda'
+  get '/jesolo', to: 'home#jesolo', as: 'jesolo'
 
   root 'home#index'
 end
